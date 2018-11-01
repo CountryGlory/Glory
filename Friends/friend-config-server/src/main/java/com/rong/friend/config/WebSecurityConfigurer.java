e33cc2e10d7080d80186fb3e6a615db445db827c
CountryGlory <2956712650@qq.com>
2018-11-01 17:18:48 +0800

    20181101 17:18添加安全验证

--- Friends/friend-config-server/src/main/java/com/rong/friend/config/WebSecurityConfigurer.java
+++ Friends/friend-config-server/src/main/java/com/rong/friend/config/WebSecurityConfigurer.java
@@ -1,20 +1,20 @@
 package com.rong.friend.config;
 
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 /**
- * 安全配置
+ * Created by zhuwj on 2018/1/8. 访问配置
  */
 @Configuration
-@EnableOAuth2Sso
+@EnableWebSecurity
 public class WebSecurityConfigurer extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-
+        // 允许所有用户访问"/"和"/home"
         http.csrf().disable();
     }
 }
--- Friends/friend-eureka-peer2/src/main/java/com/rong/friend/config/WebSecurityConfigurer.java
+++ Friends/friend-eureka-peer2/src/main/java/com/rong/friend/config/WebSecurityConfigurer.java
@@ -1,23 +1,20 @@
-// package com.rong.friend.config;
+package com.rong.friend.config;
 
-// import
-// org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
-// import org.springframework.context.annotation.Configuration;
-// import
-// org.springframework.security.config.annotation.web.builders.HttpSecurity;
-// import
-// org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-// /**
-// * 安全配置
-// */
-// @Configuration
-// @EnableOAuth2Sso
-// public class WebSecurityConfigurer extends WebSecurityConfigurerAdapter {
+/**
+ * 安全配置
+ */
+@Configuration
+@EnableOAuth2Sso
+public class WebSecurityConfigurer extends WebSecurityConfigurerAdapter {
 
-// @Override
-// protected void configure(HttpSecurity http) throws Exception {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
 
-// http.csrf().disable();
-// }
-// }
+        http.csrf().disable();
+    }
+}
